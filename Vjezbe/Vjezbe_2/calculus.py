854ea4ba4a98fa256a0d0701db65dc5c488907cc
EnaGaric <egaric@pmfst.hr>
2026-03-20 14:45:22 +0100

    Add calculus.py

---
+++ Vjezbe/Vjezbe_2/calculus.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+# two-step metoda
+def two_step(f, x, epsilon):
+    return (f(x + epsilon) - f(x)) / epsilon
+
+# three-step metoda
+def three_step(f, x, epsilon):
+    return (f(x + epsilon) - f(x - epsilon)) / (2 * epsilon)
+
+def derivacija(f, x, epsilon, metoda = "three_step"):
+    if metoda == "two_step":
+        return two_step(f, x, epsilon)
+    else:
+        return three_step(f, x, epsilon)
+    
+def raspon(f, x_min, x_max, epsilon, metoda="three_step"):
+    x_values = np.arange(x_min, x_max, epsilon)
+    y_values = []
+
+    for x in x_values:
+        y_values.append(derivacija(f, x, epsilon, metoda))
+
+    return x_values, y_values
