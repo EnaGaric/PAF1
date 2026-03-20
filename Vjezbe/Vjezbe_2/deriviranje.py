68f5ef22bb9ecbe3ffe145808917b0452c4a2be5
EnaGaric <egaric@pmfst.hr>
2026-03-20 15:05:05 +0100

    Promjena za deriviranje.py

--- Vjezbe/Vjezbe_2/deriviranje.py
+++ Vjezbe/Vjezbe_2/deriviranje.py
@@ -20,18 +20,34 @@ x_max = float(input("Unesi gornju granicu: \n"))
 epsilon = float(input("Unesi veličinu koraka (npr. 0.1 ili 0.01): "))
 
 
-x, y_three = calculus.raspon(f1, x_min, x_max, epsilon=epsilon, metoda="three-step")
-x, y_two   = calculus.raspon(f1, x_min, x_max, epsilon=epsilon, metoda="two-step")
+x1, y1_three = calculus.raspon(f1, x_min, x_max, epsilon=epsilon, metoda="three-step")
+x1, y1_two   = calculus.raspon(f1, x_min, x_max, epsilon=epsilon, metoda="two-step")
 
-y_anal = dt1(x)
+x2, y2_three = calculus.raspon(f2, x_min, x_max, epsilon=epsilon, metoda="three-step")
+x2, y2_two   = calculus.raspon(f2, x_min, x_max, epsilon=epsilon, metoda="two-step")
 
-plt.plot(x, y_anal, label="Analitičko rješenje", linewidth=2)
-plt.plot(x, y_three, '--', label=f"Three_step eps={epsilon}")
-plt.plot(x, y_two, ':', label=f"Two_step eps={epsilon}")
+y_anal1 = dt1(x1)
+y_anal2 = dt2(x2)
 
-plt.title("Derivacija funkcije x^3")
+plt.figure(figsize=(12,12))
+plt.subplot(2,1,1)
+plt.plot(x1, y_anal1, label="Analitičko", linewidth=2)
+plt.plot(x1, y1_three, '--', label="Three-step")
+plt.plot(x1, y1_two, ':', label="Two-step")
+plt.title("Derivacija x^3")
 plt.xlabel("x")
 plt.ylabel("f'(x)")
 plt.legend()
 plt.grid(True)
-plt.show()
+
+plt.subplot(2,1,2)
+plt.plot(x2, y_anal2, label="Analitičko", linewidth=2)
+plt.plot(x2, y2_three, '--', label="Three-step")
+plt.plot(x2, y2_two, ':', label="Two-step")
+plt.title("Derivacija sin(x)")
+plt.xlabel("x")
+plt.ylabel("f'(x)")
+plt.legend()
+plt.grid(True)
+
+plt.show
