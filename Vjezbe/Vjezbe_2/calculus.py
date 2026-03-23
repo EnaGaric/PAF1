dfeee705761cacc12c5a352001fef021ec65a414
EnaGaric <egaric@pmfst.hr>
2026-03-23 14:21:58 +0100

    Promjena za calculus.py

--- Vjezbe/Vjezbe_2/calculus.py
+++ Vjezbe/Vjezbe_2/calculus.py
@@ -22,3 +22,32 @@ def raspon(f, x_min, x_max, epsilon, metoda="three_step"):
         y_values.append(derivacija(f, x, epsilon, metoda))
 
     return x_values, np.array(y_values)
+
+def pravokutna_aproksimacija(f, a, b, n):
+    dx = (b - a) / n
+    donja_granica = 0
+    gornja_granica = 0
+
+    for i in range(n):
+        x1 = a + dx * i
+        x2 = x1 + dx
+
+        f1 = f(x1)
+        f2 = f(x2)
+
+        donja_granica += min(f1, f2) * dx
+        gornja_granica += max(f1, f2) * dx
+
+    return donja_granica, gornja_granica
+    
+def trapezna_metoda(f, a, b, n):
+    dx = (b - a) / n
+    suma = 0
+
+    for i in range(n):
+        x1 = a + dx * i
+        x2 = x1 + dx
+
+        suma += (f(x1) + f(x2)) / 2 * dx
+    
+    return suma
